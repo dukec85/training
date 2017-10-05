2284e743abf4398af870683cdc933ca42434595f
Chris <cd.duke@gmail.com>
2017-10-05 18:04:29 -0500

    add loops

--- src/basics/Cities.java
+++ src/basics/Cities.java
@@ -10,18 +10,9 @@ public class Cities {
 		System.out.println(cities[2]);
 		System.out.println(cities[3]);
 		
-		// Declare and define the array (only size) 
-		String[] states = new String[5];
-		states[0] = "California";
-		states[1] = "Tennessee";
-		states[2] = "Utah";
-		states[3] = "Vermont";
-		states[4] = "Ohio";
-		System.out.println(states[0]);
-		
 		// Declare array
 		String[] countries; 
-		
+				
 		// Define the array
 		countries = new String[3];
 		countries[0] = "USA";
@@ -29,6 +20,36 @@ public class Cities {
 		countries[2] = "UK";
 		System.out.println(countries[2]);
 		
+		System.out.println("****************");
+				
+		// Declare and define the array (only size) 
+		String[] states = new String[5];
+		states[0] = "California";
+		states[1] = "Tennessee";
+		states[2] = "Utah";
+		states[3] = "Vermont";
+		states[4] = "Ohio";
+		int i = 0;
+		
+		// Do Loop: enters the loop THEN tests the condition
+		do {
+			System.out.println("STATE: " + states[i]);
+			i = i + 1;
+		} while (i < 5);
+		
+		
+		// While Loop: tests condition first the enters loop
+		int n = 0;
+		while (n <= 4) {
+			System.out.println("STATE at " + n + ": " + states[n]);
+			n++;
+		}
+		
+		System.out.println("\nPRINTING WITH FOR LOOP");
+		// For Loop: best structure for iterating through an array
+		for (int x = 0; x < 5; x++) {
+			System.out.println(states[x]);
+		}
 	}
 
 }
